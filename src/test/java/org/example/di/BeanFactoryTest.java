ae83179896fe120ba2c7e59e05b7fca190ccc918
illyoung22 <illyoung22@hanmail.net>
2023-12-03 12:54:08 +0900

    패스트강의 di

--- src/test/java/org/example/di/BeanFactoryTest.java
+++ src/test/java/org/example/di/BeanFactoryTest.java
@@ -2,12 +2,15 @@ package org.example.di;
 
 import org.example.annotation.Controller;
 import org.example.annotation.Service;
+import org.example.controller.UserController;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.reflections.Reflections;
 
 import java.lang.annotation.Annotation;
 import java.util.HashSet;
 import java.util.Set;
+import  static org.assertj.core.api.Assertions.assertThat;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -29,5 +32,13 @@ class BeanFactoryTest {
             beans.addAll(reflections.getTypesAnnotatedWith(annotation));
         }
         return beans;
+    }
+
+    @Test
+    void diTest() {
+        UserController userController = beanFactory.getBean(UserController.class);
+
+        assertThat(userController).isNotNull();
+        assertThat(userController.getUserService()).isNotNull();
     }
 }
